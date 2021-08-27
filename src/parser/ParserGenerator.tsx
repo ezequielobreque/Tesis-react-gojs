d05b50514eedae4d5ec73cc894ea4448b720ea5d
ezequielobreque <ezequielobreque777@gmail.com>
2021-08-27 13:26:02 -0300

    Cambios a los errores

--- src/parser/ParserGenerator.tsx
+++ src/parser/ParserGenerator.tsx
@@ -10,106 +10,88 @@ import "codemirror/addon/edit/matchbrackets";
 import "codemirror/keymap/sublime";
 import "codemirror/theme/dracula.css";
 import { stringExample } from "./example";
-import { onInputEditorContentChange } from "./errors";
+import { FindErrors, onInputEditorContentChange } from "./errors";
 import _ from "lodash";
 /*interface IParserGenerator {
   text: string;
 }*/
 
 export const ParserGenerator = ({ changeStateCallback, nodeData }: any) => {
-  /*const createDataNodes=()=>{
+	/*const createDataNodes=()=>{
     changeStateCallback();
   }*/
-  let action: ActionsFunctionsClass = useMemo(
-    () => new ActionsFunctionsClass(),
-    []
-  );
+	let action: ActionsFunctionsClass = useMemo(() => new ActionsFunctionsClass(), []);
 
-  //let action:ActionsFunctionsClass=new ActionsFunctionsClass()
-  /* console.log(changeStateCallback);
+	//let action:ActionsFunctionsClass=new ActionsFunctionsClass()
+	/* console.log(changeStateCallback);
   console.log(nodeData);*/
-  const [state, setState] = useState<any>();
-  const [stateResult, setStateResult] = useState<any>();
- 
-  // const [texto, setTexto] = useState<string>("COMPOSITE domicilio {ciudad,calle,numero};"
-  // +" RELTIONSHIP compro{    factura (1, 1),    persona (1, N)};"
-  // +"ENTITY proveedor CHILD OF persona{cuit IDENTIFIER };"
-  // +"ENTITY persona COVERAGE (PARTIAL, OVERLAY){documento IDENTIFIER, cuil IDENTIFIER,nacimiento,apellido,nombre,    celular (0,1),   domicilio  }");
-  const [texto, setTexto] = useState<string>(stringExample);
-  const handleSubmit = (
-    lexAndParseResult: any,
-    instance: CodeMirror.Editor
-  ) => {
-    try {
-      console.log(
-        "🚀 ~ file: ParserGenerator.tsx ~ line 27 ~ handleSubmit ~ ñexAndParseResult.lexErrors.length",
-        lexAndParseResult.lexErrors.length
-      );
-      assert.strictEqual(lexAndParseResult.lexErrors.length, 0);
-      console.log(
-        "🚀 ~ file: ParserGenerator.tsx ~ line 29 ~ handleSubmit ~ lexAndParseResult.parseErrors",
-        lexAndParseResult.parseErrors
-      );
+	const [state, setState] = useState<any>();
+	const [stateResult, setStateResult] = useState<any>();
+	const [inputEditorMarker, setInputEditorMarker] = useState<any[]>([]);
+	// const [texto, setTexto] = useState<string>("COMPOSITE domicilio {ciudad,calle,numero};"
+	// +" RELTIONSHIP compro{    factura (1, 1),    persona (1, N)};"
+	// +"ENTITY proveedor CHILD OF persona{cuit IDENTIFIER };"
+	// +"ENTITY persona COVERAGE (PARTIAL, OVERLAY){documento IDENTIFIER, cuil IDENTIFIER,nacimiento,apellido,nombre,    celular (0,1),   domicilio  }");
+	const [texto, setTexto] = useState<string>(stringExample);
+	const handleSubmit = (lexAndParseResult: any, instance: CodeMirror.Editor) => {
+		try {
+			console.log("🚀 ~ file: ParserGenerator.tsx ~ line 27 ~ handleSubmit ~ ñexAndParseResult.lexErrors.length", lexAndParseResult.lexErrors.length);
+			assert.strictEqual(lexAndParseResult.lexErrors.length, 0);
+			console.log("🚀 ~ file: ParserGenerator.tsx ~ line 29 ~ handleSubmit ~ lexAndParseResult.parseErrors", lexAndParseResult.parseErrors);
 
-      assert.strictEqual(lexAndParseResult.parseErrors.length, 0);
-      console.log(
-        "🚀 ~ file: ParserGenerator.tsx ~ line 32 ~ handleSubmit ~ lexAndParseResult",
-        lexAndParseResult
-      );
-    } catch (e) {
-      console.log(e);
-    }
-    if (lexAndParseResult.cst) {
-      const result = action.ActionsFunctions(state);
-      if (result) {
-        setState(lexAndParseResult.cst.children);
-        stateResult(result);
-      }
-    }
-  };
-  const [inputEditorMarker, setInputEditorMarker] = useState<any[]>([]);
-  const debo = useRef(_.debounce(handleSubmit, 2000));
-  //{ "arr": [1,2,3], "obj": {"num":666}}
-  const handleChange = (instance: CodeMirror.Editor, change: any) => {
-    const lexAndParseResult = parseJson(instance.getValue());
+			assert.strictEqual(lexAndParseResult.parseErrors.length, 0);
+			console.log("🚀 ~ file: ParserGenerator.tsx ~ line 32 ~ handleSubmit ~ lexAndParseResult", lexAndParseResult);
+		} catch (e) {
+			console.log(e);
+		}
+		if (lexAndParseResult.cst) {
+			const FindRepeatsWords = FindErrors(instance, inputEditorMarker, setInputEditorMarker, lexAndParseResult.cst.children);
+			if (FindRepeatsWords===0) {
+				const result = action.ActionsFunctions(lexAndParseResult.cst.children);
+				if (result) {
+					//setState(lexAndParseResult.cst.children);
+					setStateResult(result);
+				}
+			}
+		}
+	};
 
-    onInputEditorContentChange(
-      instance,
-      inputEditorMarker,
-      setInputEditorMarker,
-      lexAndParseResult
-    );
-    debo.current(lexAndParseResult, instance);
-  };
-  useEffect(() => {
-    if (stateResult) {
-      changeStateCallback(stateResult);
-    }
-  }, [stateResult]);
-  // const ref = useRef(initialValue)
+	const debo = useRef(_.debounce(handleSubmit, 2000));
+	//{ "arr": [1,2,3], "obj": {"num":666}}
+	const handleChange = (instance: CodeMirror.Editor, change: any) => {
+		const lexAndParseResult = parseJson(instance.getValue());
+		onInputEditorContentChange(instance, inputEditorMarker, setInputEditorMarker, lexAndParseResult);
+		debo.current(lexAndParseResult, instance);
+	};
+	useEffect(() => {
+		if (stateResult) {
+			changeStateCallback(stateResult);
+		}
+	}, [stateResult]);
+	// const ref = useRef(initialValue)
 
-  return (
-    <div>
-      <label className="col-12">Name:</label>
-      <div className="parser-div">
-        {/* <textarea
+	return (
+		<div>
+			<label className="col-12">Name:</label>
+			<div className="parser-div">
+				{/* <textarea
             style={{width:'100%'}}
             value={texto}
             
             onChange={handleChange}
           /> */}
-        <CodeMirror
-          value={texto}
-          onChange={handleChange}
-          options={{
-            mode: "text",
-            lineNumbers: true,
-            theme: "dracula",
-            tabSize: 1,
-          }}
-        />
-        {/* <input type="submit" value="Submit" className="submit-btn"/> */}
-      </div>
-    </div>
-  );
+				<CodeMirror
+					value={texto}
+					onChange={handleChange}
+					options={{
+						mode: "text",
+						lineNumbers: true,
+						theme: "dracula",
+						tabSize: 1,
+					}}
+				/>
+				{/* <input type="submit" value="Submit" className="submit-btn"/> */}
+			</div>
+		</div>
+	);
 };
--- src/parser/errors.ts
+++ src/parser/errors.ts
@@ -1,156 +1,134 @@
 import _ from "lodash";
-import {
-  Children6,
-  Children9,
-  Composite,
-  Entity,
-  PropertyEntity,
-  Relation,
-  Relationship,
-  RootObject,
-} from "./IParse";
-export function markInputErrors(
-  lexErrors: any,
-  parseErrors: any,
-  inputEditor: any,
-  inputEditorMark: any[],
-  setInputMarket: any
-) {
-  var start, end, marker;
-  let inputEditorMarkers: any[] = inputEditorMark;
-  _.forEach(inputEditorMarkers, function (currMarker) {
-    currMarker.clear();
-  });
-  inputEditorMarkers = [];
-  _.forEach(lexErrors, function (currLexError) {
-    start = { line: currLexError.line - 1, ch: currLexError.column - 1 };
-    end = {
-      line: currLexError.line - 1,
-      ch: currLexError.column - 1 + currLexError.length,
-    };
-    marker = inputEditor.markText(start, end, {
-      className: "markTextError",
-      title: currLexError.message,
-    });
-    inputEditorMarkers.push(marker);
-  });
+import { Children6, Children9, Composite, Entity, PropertyEntity, Relation, Relationship, RootObject } from "./IParse";
+export function markInputErrors(lexErrors: any, parseErrors: any, inputEditor: any, inputEditorMark: any[], setInputMarket: any) {
+	var start, end, marker;
+	let inputEditorMarkers: any[] = inputEditorMark;
+	_.forEach(inputEditorMarkers, function (currMarker) {
+		currMarker.clear();
+	});
+	inputEditorMarkers = [];
+	_.forEach(lexErrors, function (currLexError) {
+		start = { line: currLexError.line - 1, ch: currLexError.column - 1 };
+		end = {
+			line: currLexError.line - 1,
+			ch: currLexError.column - 1 + currLexError.length,
+		};
+		marker = inputEditor.markText(start, end, {
+			className: "markTextError",
+			title: currLexError.message,
+		});
+		inputEditorMarkers.push(marker);
+	});
 
-  _.forEach(parseErrors, function (currParserError) {
-    start = {
-      line: currParserError.token.startLine - 1,
-      ch: currParserError.token.startColumn - 1,
-    };
+	_.forEach(parseErrors, function (currParserError) {
+		start = {
+			line: currParserError.token.startLine - 1,
+			ch: currParserError.token.startColumn - 1,
+		};
 
-    var lastToken = currParserError.token;
-    if (!_.isEmpty(currParserError.resyncedTokens)) {
-      const x: any = _.max(currParserError.resyncedTokens);
-      lastToken = x.startOffset;
-    }
+		var lastToken = currParserError.token;
+		if (!_.isEmpty(currParserError.resyncedTokens)) {
+			const x: any = _.max(currParserError.resyncedTokens);
+			lastToken = x.startOffset;
+		}
 
-    end = {
-      line: lastToken.endLine
-        ? lastToken.endLine - 1
-        : // assume startLine === endLine if we endLine is not tracked
-          lastToken.startLine - 1,
-      ch: lastToken.endColumn
-        ? lastToken.endColumn
-        : // compute the endColumn ourselves
-          lastToken.startColumn + lastToken.image.length,
-    };
+		end = {
+			line: lastToken.endLine
+				? lastToken.endLine - 1
+				: // assume startLine === endLine if we endLine is not tracked
+				  lastToken.startLine - 1,
+			ch: lastToken.endColumn
+				? lastToken.endColumn
+				: // compute the endColumn ourselves
+				  lastToken.startColumn + lastToken.image.length,
+		};
 
-    marker = inputEditor.markText(start, end, {
-      className: "markTextError",
-      title: currParserError.message,
-    });
-    inputEditorMarkers.push(marker);
-  });
-  console.log(inputEditorMarkers);
-  setInputMarket(inputEditorMarkers);
+		marker = inputEditor.markText(start, end, {
+			className: "markTextError",
+			title: currParserError.message,
+		});
+		inputEditorMarkers.push(marker);
+	});
+	console.log(inputEditorMarkers);
+	setInputMarket(inputEditorMarkers);
 }
 //inputEditor.on("change", _.debounce(onInputEditorContentChange, 250))
-export function onInputEditorContentChange(
-  inputEditor: any,
-  inputEditorMarkers: any,
-  setInputMarket: any,
-  lex: any
-) {
-  var parseResult, printResult;
+export function onInputEditorContentChange(inputEditor: any, inputEditorMarkers: any, setInputMarket: any, lex: any) {
+	var parseResult, printResult;
 
-  // function lex(text) {
-  //     var lexResult = lexer.tokenize(text)
-  //     return lexResult
-  // }
+	// function lex(text) {
+	//     var lexResult = lexer.tokenize(text)
+	//     return lexResult
+	// }
 
-  // function parse(lexResult, startRuleName) {
-  //     parser.reset()
-  //     parser.input = lexResult.tokens
-  //     var value = parser[startRuleName]()
-  //     return {value: value, parseErrors: parser.errors}
-  // }
+	// function parse(lexResult, startRuleName) {
+	//     parser.reset()
+	//     parser.input = lexResult.tokens
+	//     var value = parser[startRuleName]()
+	//     return {value: value, parseErrors: parser.errors}
+	// }
 
-  // parserOutput.setValue("")
-  // var lexResult = lex(inputEditor.getValue())
-  // may be falsy if the example is for the lexer only
+	// parserOutput.setValue("")
+	// var lexResult = lex(inputEditor.getValue())
+	// may be falsy if the example is for the lexer only
 
-  markInputErrors(
-    lex.lexErrors,
-    lex.parseErrors,
-    inputEditor,
-    inputEditorMarkers,
-    setInputMarket
-  );
+	markInputErrors(lex.lexErrors, lex.parseErrors, inputEditor, inputEditorMarkers, setInputMarket);
 }
-export function FindErrors(
-  codemirror: any,
-  inputEditorMark: any[],
-  setInputMarket: any,
-  datafind: any
-) {
-  let inputEditorMarkers: any[] = inputEditorMark;
-  _.forEach(inputEditorMarkers, function (currMarker) {
-    currMarker.clear();
-  });
-  inputEditorMarkers = [];
-  const data: RootObject = datafind;
+export function FindErrors(codemirror: any, inputEditorMark: any[], setInputMarket: any, datafind: any) {
+	let inputEditorMarkers: any[] = inputEditorMark;
+	_.forEach(inputEditorMarkers, function (currMarker) {
+		currMarker.clear();
+	});
+	inputEditorMarkers = [];
+	const data: RootObject = datafind;
+  console.log(data);
+	let Entity = data.Entity;
+	const Relationship = data.Relationship || [];
+	const Composite = data.Composite || [];
+	let mapOfObjects = new Map<string, object>();
+	let mapOfRepeatsObjects = new Map<string, object>();
+	let repeat = 0;
+	Composite.map((x: Composite) => {
+		if (mapOfObjects.has(x.children.StringEntity[0].image)) {
+			mapOfRepeatsObjects.set(x.children.StringEntity[0].image, x);
+		} else {
+			mapOfObjects.set(x.children.StringEntity[0].image, x);
+		}
+	});
+	Entity.map((x: Entity) => {
+		if (mapOfObjects.has(x.children.StringEntity[0].image)) {
+			mapOfRepeatsObjects.set(x.children.StringEntity[0].image, x);
+		} else {
+			mapOfObjects.set(x.children.StringEntity[0].image, x);
+		}
+	});
+	Relationship.map((x: Relationship) => {
+		if (mapOfObjects.has(x.children.StringEntity[0].image)) {
+			mapOfRepeatsObjects.set(x.children.StringEntity[0].image, x);
+		} else {
+			mapOfObjects.set(x.children.StringEntity[0].image, x);
+		}
+	});
+	mapOfRepeatsObjects.forEach((x: any) => {
+    const start = {
+			line: x.children.StringEntity[0].startLine - 1,
+			ch: x.children.StringEntity[0].startColumn - 1,
+		};
 
-  let Entity = data.Entity;
-  const Relationship = data.Relationship || [];
-  const Composite = data.Composite || [];
-  let mapOfObjects = new Map<string, object>();
-  let mapOfRepeatsObjects = new Map<string, object>();
-  let repeat = 0;
-  Composite.map((x: Composite) => {
-    if (mapOfObjects.has(x.children.StringEntity[0].image)) {
-      mapOfRepeatsObjects.set(x.children.StringEntity[0].image, x);
-    } else {
-      mapOfObjects.set(x.children.StringEntity[0].image, x);
-    }
-  });
-  Entity.map((x: Entity) => {
-    if (mapOfObjects.has(x.children.StringEntity[0].image)) {
-      mapOfRepeatsObjects.set(x.children.StringEntity[0].image, x);
-    } else {
-      mapOfObjects.set(x.children.StringEntity[0].image, x);
-    }
-  });
-  Relationship.map((x: Relationship) => {
-    if (mapOfObjects.has(x.children.StringEntity[0].image)) {
-      mapOfRepeatsObjects.set(x.children.StringEntity[0].image, x);
-    } else {
-      mapOfObjects.set(x.children.StringEntity[0].image, x);
-    }
-  });
-  mapOfRepeatsObjects.forEach((x: any) => {
-    const marker = codemirror.markText(
-      x.children.StringEntity[0].startLine,
-      x.children.StringEntity[0].endLine,
-      {
-        className: "markTextError",
-        title: "Elemento repetido",
-      }
-    );
-    inputEditorMarkers.push(marker);
-  });
+	
 
-  setInputMarket(inputEditorMarkers);
+		const end = {
+      line: x.children.StringEntity[0].endLine - 1,
+			ch: x.children.StringEntity[0].endColumn,
+		};
+    
+		const marker = codemirror.markText(start,end, {
+			className: "markTextError",
+			title: "Elemento repetido",
+		});
+		inputEditorMarkers.push(marker);
+	});
+
+	setInputMarket(inputEditorMarkers);
+  return mapOfRepeatsObjects.size;
 }
